fb42c42c7af61fcba9bb21f1885dacc9ffed7358
woojin-heo <woojin.heo@gmail.com>
2025-01-07 11:26:07 +0800

    document gpt without memory

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='FullstackGPT Home',
+    page_icon="🤖"
+)
+st.title("FullstackGPT Home")
+
+with st.sidebar:
+    st.title('sidebar title')
+    # st.text_input('')
